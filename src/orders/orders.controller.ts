65540c1ba8328c6af9accd1938a2875d862f93c0
Zhanna Skaraieva <i.m.p.h.e.l.l@gmail.com>
2025-10-29 07:36:21 +0200

    add order controller

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -1,19 +1,20 @@
-import { Controller, Post, Get, Body, Req } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
 import { OrdersService } from './orders.service';
-import type { Request } from 'express';
+
 import { CreateOrderDto } from './dto/CreateOrderDto.dto';
+import { Order } from '@prisma/client';
 
 @Controller('orders')
 export class OrdersController {
   constructor(private readonly ordersService: OrdersService) {}
 
   @Post()
-  createOrder(@Body() createOrderDto: CreateOrderDto, @Req() req: Request) {
-    // Logic to create an order from the user's cart and process payment
+  createOrder(@Body() dto: CreateOrderDto): Promise<Order> {
+    return this.ordersService.createOrder(dto);
   }
 
-  @Get()
-  getOrders(@Req() req: Request) {
-    // Logic to retrieve all orders for the authenticated user
-  }
+  // @Get()
+  // getOrders(@Req() req: Request) {
+  //   // Logic to retrieve all orders for the authenticated user
+  // }
 }
